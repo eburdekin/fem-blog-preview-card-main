81a9cbd81e0f5f0f31681cbf71ca1c75975b6f2a
Eileen <eileenburdekin@proton.me>
2024-01-10 15:15:40 -0800

    initial styles, add readme template

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,24 @@ module.exports = {
     fontFamily: {
       sans: ["Figtree", "sans-serif"],
     },
+    fontWeight: {
+      semibold: 800, // Adjust if Figtree's semi-bold weight differs
+    },
     container: {
       center: true,
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          yellow: "hsl(47, 88%, 63%)",
+        },
+        neutral: {
+          white: "hsl(0, 0%, 100%)",
+          gray: "hsl(0, 0%, 50%)",
+          black: "hsl(0, 0%, 7%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
